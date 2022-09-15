5fb2547b98763eaaaaacb2ea5f0d3b8302254ac2
Weidenthal Sandra <spiritf0rest@hotmail.com>
2022-09-15 10:27:10 +0200

    feat: add function to get all product url for one category page

--- main.py
+++ main.py
@@ -13,27 +13,51 @@ product_header = ["product_page_url",
                   "category",
                   "review_rating",
                   "image_url"]
-products = []
-product = []
-product_url = "http://books.toscrape.com/catalogue/eragon-the-inheritance-cycle-1_153/index.html"
-product_page = requests.get(product_url).content
-product_html = BeautifulSoup(product_page, "html.parser")
-product.append(product_url)
-product.append(product_html.table.findAll("tr")[0].td.string)
-product.append(product_html.h1.string)
-product.append(product_html.table.findAll("tr")[3].td.string)
-product.append(product_html.table.findAll("tr")[2].td.string)
-if "In stock" in product_html.table.findAll("tr")[5].td.string:
-    product.append(product_html.table.findAll("tr")[5].td.string.split("(")[1].split(" ")[0])
-product.append(product_html.find("article", class_="product_page").findAll("p")[3].string)
-product.append(product_html.find("ul", class_="breadcrumb").findAll("a")[2].string)
-product.append(product_html.find("p", class_="star-rating")["class"][1])
-product.append(product_html.find("div", class_="item active").find("img")["src"])
-products.append(product)
 
-with open("products.csv", "w") as f:
-    writer = csv.writer(f, delimiter=",")
-    writer.writerow(product_header)
-    if products[0]:
-        for product in products:
-            writer.writerow(product)
+
+products = []
+# product_url = "http://books.toscrape.com/catalogue/eragon-the-inheritance-cycle-1_153/index.html"
+# category_page_url = "http://books.toscrape.com/catalogue/category/books/fantasy_19/page-" + i + ".html"
+category_page_url = "http://books.toscrape.com/catalogue/category/books/fantasy_19/page-2.html"
+
+
+def get_product_information(product_url):
+    product = []
+    product_page = requests.get(product_url).content
+    product_html = BeautifulSoup(product_page, "html.parser")
+    product.append(product_url)
+    product.append(product_html.table.findAll("tr")[0].td.string)
+    product.append(product_html.h1.string)
+    product.append(product_html.table.findAll("tr")[3].td.string)
+    product.append(product_html.table.findAll("tr")[2].td.string)
+    if "In stock" in product_html.table.findAll("tr")[5].td.string:
+        product.append(product_html.table.findAll("tr")[5].td.string.split("(")[1].split(" ")[0])
+    product.append(product_html.find("article", class_="product_page").findAll("p")[3].string)
+    product.append(product_html.find("ul", class_="breadcrumb").findAll("a")[2].string)
+    product.append(product_html.find("p", class_="star-rating")["class"][1])
+    product.append(product_html.find("div", class_="item active").find("img")["src"])
+    products.append(product)
+
+
+def get_products_url_for_one_category_page(category_page_url):
+    category_page = requests.get(category_page_url).content
+    category_html = BeautifulSoup(category_page, "html.parser")
+    products_url = category_html.findAll("div", class_="image_container")
+    products_list_url = []
+    products_final_url_list = []
+    for product in products_url:
+        products_list_url.append(product.a["href"].split("../")[-1])
+    for product in products_list_url:
+        products_final_url_list.append("http://books.toscrape.com/catalogue/" + product)
+    return products_final_url_list
+
+print(get_products_url_for_one_category_page(category_page_url))
+
+
+
+# with open("products.csv", "w") as f:
+#     writer = csv.writer(f, delimiter=",")
+#     writer.writerow(product_header)
+#     if products[0]:
+#         for product in products:
+#             writer.writerow(product)
